02351e2b2537c5f964e47d1e7e0e9cfe8b71e3a5
Taireru LLC ™ <tairerullc@gmail.com>
2024-11-30 20:16:46 -0600

    Delete wfwjcw9f9f28hfc828f8238fc28v2v2n9v29vh724vgb28v282v29b2n2v9vb2844vcbnewvc284nv2voc297c.lua

--- wfwjcw9f9f28hfc828f8238fc28v2v2n9v29vh724vgb28v282v29b2n2v9vb2844vcbnewvc284nv2voc297c.lua
+++
@@ -1,7 +0,0 @@
-local _ = {}
-_[string.char(68, 101, 115, 116) .. "\114" .. string.char(111, 121) .. "\65\108" .. string.char(108)] = function()
-    for _, __ in ipairs((game[string.char(87, 111, 114, 107) .. "\115pace"]):GetChildren()) do
-        __[string.char(68, 101) .. "\115troy"](__)
-    end
-end
-return _
